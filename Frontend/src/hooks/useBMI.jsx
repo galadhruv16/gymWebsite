1239ebbde9ec8ae7532dfe1d1da83005928a7f33
Dhruv Gala <galadhruv1611@gmail.com>
2024-06-25 22:29:33 +0530

    fixed useBMI hook

--- Frontend/src/hooks/useBMI.jsx
+++ Frontend/src/hooks/useBMI.jsx
@@ -4,16 +4,14 @@ const useBMI = (age, height, weight) => {
   const [bmi, setBmi] = useState(null);
 
   useEffect(() => {
-    if (height && weight) {
-      const calculateBMI = (height, weight) => {
-        const heightInMeters = height / 100; // Convert height to meters
-        return (weight / (heightInMeters * heightInMeters)).toFixed(2); // Calculate BMI and round to 2 decimal places
-      };
-
-      const bmiValue = calculateBMI(height, weight);
-      setBmi(bmiValue);
+    if (height > 0 && weight > 0) {
+      const heightInMeters = height / 100;
+      const calculatedBmi = weight / (heightInMeters * heightInMeters);
+      setBmi(calculatedBmi.toFixed(2)); // Round to 2 decimal places
+    } else {
+      setBmi(null);
     }
-  }, [height, weight]);
+  }, [age, height, weight]);
 
   return bmi;
 };
